7ec54434d8d2710b8936cf801adfb90411374956
Manuel Fernandez <manuelfrnandez@gmail.com>
2020-07-27 23:23:54 -0400

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,7 @@
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('banner'),
     renderer: 'canvas',
-    loop: false,
+    loop: true,
     autoplay: true,
     path: 'json/data.json'
   })
