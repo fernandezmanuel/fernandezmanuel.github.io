4dd30ac439537e464fbe8df8a5b842b9f941f80d
Manuel Fernandez <manuelfrnandez@gmail.com>
2020-08-17 15:59:39 -0400

    Update location.js

--- js/location.js
+++ js/location.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('location'),
-    renderer: 'svg',
+    renderer: 'canvas',
     loop: false,
     autoplay: true,
     path: 'json/previous_location.json'
